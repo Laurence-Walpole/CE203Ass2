6e9306ccdfb8add11ad19d766a1f110476374b11
Laurence-Walpole <me@laurence-walpole.me>
2018-12-03 14:43:14 +0000

    Updated

--- src/engine/utilities/Loader.java
+++ src/engine/utilities/Loader.java
@@ -1,17 +1,38 @@
 package engine.utilities;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Loader {
 
-    public static void loadItems(){
+    public static void loadEntities(){
+        File file = new File("C:\\Users\\isair\\IdeaProjects\\CE203Ass2\\entities.conf");
 
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+
+            String line = "";
+            while((line = reader.readLine()) != null){
+                if (line.startsWith("#")) continue;
+
+                handleInput(line.split(","));
+
+            }
+
+        }catch(IOException e){
+            System.err.println(e);
+        }
     }
 
-    public static void loadNPCs(){
+    private static void handleInput(String[] components){
+        String type = components[0];
 
-    }
+        switch(type){
+            case "WEAPON":
 
-    public static void loadPlayer(){
-
+        }
     }
 
     public static void loadMap(){
